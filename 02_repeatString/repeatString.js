4847c761fc1e19cf9888bde01d243b3d51e1b244
xxh296 <140650758+xxh296@users.noreply.github.com>
2024-12-21 09:21:13 -0800

    Complete '02_repeatString'
    Wrote a function to repeat a string for a given amount of times (per assignment).
    Read Jest code, toggled tests on/off.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,24 @@
-const repeatString = function() {
+// const repeatString = function() {
 
-};
+// };
+
+// let result;
+
+function repeatString(string, num) {
+    if (num <0) {
+        return "ERROR";
+    }
+    
+    let result = "";
+    for (let i = 0; i < num; i++) {
+        result += string;
+    }
+    return result;
+}
+
+//repeatString("hey", 3);
+//DEBUG
+// console.log(result);
 
 // Do not edit below this line
 module.exports = repeatString;

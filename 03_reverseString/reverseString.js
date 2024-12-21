2cc114dbf1bfd7efcdd77b4cc33a0addbe3f9382
xxh296 <140650758+xxh296@users.noreply.github.com>
2024-12-21 13:12:28 -0800

    Complete '03_reverseString' Solved with 'split("")', 'reverse()', and 'join("")'.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,6 @@
-const reverseString = function() {
-
-};
+const reverseString = function(stringToReverse) {
+    return stringToReverse.split("").reverse().join("");
+    }
 
 // Do not edit below this line
 module.exports = reverseString;

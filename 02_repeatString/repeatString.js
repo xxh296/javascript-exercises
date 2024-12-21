af4d3c99e0369df4aa6bb751db66c1e3e5f1007c
xxh296 <140650758+xxh296@users.noreply.github.com>
2024-12-21 10:44:11 -0800

    Optimize '02-repeatString' Improved the clarity of variables, shortened the simple 'if'.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,11 +1,9 @@
-function repeatString(string, num) {
-    if (num <0) {
-        return "ERROR";
-    }
+function repeatString(stringToRepeat, numberOfTimes) {
+    if (numberOfTimes < 0) return "ERROR";
     
     let result = "";
-    for (let i = 0; i < num; i++) {
-        result += string;
+    for (let i = 0; i < numberOfTimes; i++) {
+        result += stringToRepeat;
     }
     return result;
 }

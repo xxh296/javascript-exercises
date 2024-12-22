627c50576b02fb37799bc2bf4439d5caddfb584a
xxh296 <140650758+xxh296@users.noreply.github.com>
2024-12-21 23:26:12 -0800

    Complete '07_tempConversion' Used conversion formulas and rounding to single decimal.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  temperature = (temperature - 32) * (5/9);
+  return temperature = Math.round(temperature * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  temperature = temperature * (9/5) + 32;
+  return temperature = Math.round(temperature * 10) / 10;
 };
 
 // Do not edit below this line

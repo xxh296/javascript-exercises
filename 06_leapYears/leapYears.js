6ff70437eeea4852d2eaa8a7cad88d06ef8dfea1
xxh296 <140650758+xxh296@users.noreply.github.com>
2024-12-21 22:47:14 -0800

    Complete '06_leapYears'

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,28 @@
-const leapYears = function() {
-
+const leapYears = function(year) {    
+    if (year%100==0 && year%400==0) return true;
+    if (year%100==0 && year%400!=0) return false;
+    if (year%4==0) return true;
+    return false;
 };
+
+// Second (longer) option:
+/*
+const leapYears = function(year) {
+    if (year%100==0) {
+        if (year%400==0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    if (year%4==0) {
+        return true;
+    } else {
+        return false;
+    }
+};
+*/
 
 // Do not edit below this line
 module.exports = leapYears;

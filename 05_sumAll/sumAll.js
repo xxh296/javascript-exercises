760e7af4f13d2003a0e4ed6db9386eff76010688
xxh296 <140650758+xxh296@users.noreply.github.com>
2024-12-21 21:22:00 -0800

    Complete first 5 cases, '05_solveAll' Completed the following functionalities: * sum for a range * work with large numbers * handle the situation where a larger number is entered first * show 'ERROR', if a negative number is used * show 'ERROR' for a non-integer input

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, lastNumber) {
+    // formula : (n / 2) * (firstNumber + lastNumber)
+    // where n is the number of numbers in the range
+    // needs +1 correction, as most ranges don't start with 0
+    // TODO: a conditional to include 0
+
+    if (firstNumber < 0 || lastNumber < 0) return "ERROR";
+    if (!Number.isInteger(firstNumber+lastNumber)) return "ERROR"; 
+
+    if (firstNumber > lastNumber) {
+        const tempNumber = firstNumber;
+        firstNumber = lastNumber;
+        lastNumber = tempNumber;
+    }
+    //debug
+    // console.log(lastNumber);
+
+    return ((lastNumber - firstNumber + 1) / 2) * (firstNumber + lastNumber);
+
+
 
 };
+
+// const sum = sumAll(15, 10);
+// console.log(sum);
 
 // Do not edit below this line
 module.exports = sumAll;

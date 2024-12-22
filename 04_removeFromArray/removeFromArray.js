0022b3df807e3285cf184e9a2e8eb1fccdd8ba89
xxh296 <140650758+xxh296@users.noreply.github.com>
2024-12-21 17:31:15 -0800

    Add ability to remove one arg Added a loop in assignment '04_removeFromArray', so that the function could remove a single argument. TODO: The function should be able to remove more arguments, if supplied.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayToTrim, a) {
+    // Passes 1st test only
+    // TODO - take any number of args
+    //
+    for (i=0; i<arrayToTrim.length; i++) {
+        if(arrayToTrim[i] === a) arrayToTrim.splice(i, 1);
+    }
+    return arrayToTrim;
 };
+
+//DEBUG
+let boo = removeFromArray([1, 2, 3, 4, 5, "hey"], 7);
+console.log(boo);
 
 // Do not edit below this line
 module.exports = removeFromArray;

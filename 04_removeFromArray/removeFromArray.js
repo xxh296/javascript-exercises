c017eed4a83e776d023175723d9af7e6fb76edd0
xxh296 <140650758+xxh296@users.noreply.github.com>
2024-12-21 20:11:29 -0800

    Complete '04_removeFromArray' Refactored to use the 'filter()' method.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,16 +1,6 @@
-const removeFromArray = function(arrayToTrim, a) {
-    // Passes 1st test only
-    // TODO - take any number of args
-    //
-    for (i=0; i<arrayToTrim.length; i++) {
-        if(arrayToTrim[i] === a) arrayToTrim.splice(i, 1);
-    }
-    return arrayToTrim;
-};
-
-//DEBUG
-let boo = removeFromArray([1, 2, 3, 4, 5, "hey"], 7);
-console.log(boo);
+const removeFromArray = function(arrayToTrim, ...itemsToRemove) {
+    return arrayToTrim.filter(item => !itemsToRemove.includes(item));
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;

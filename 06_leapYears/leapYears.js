2ddb57afaccb741b18a70ca4b46277a536fa61dc
xxh296 <140650758+xxh296@users.noreply.github.com>
2024-12-21 22:51:06 -0800

    Remove comments in '06_leapYears'

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -5,24 +5,5 @@ const leapYears = function(year) {
     return false;
 };
 
-// Second (longer) option:
-/*
-const leapYears = function(year) {
-    if (year%100==0) {
-        if (year%400==0) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    if (year%4==0) {
-        return true;
-    } else {
-        return false;
-    }
-};
-*/
-
 // Do not edit below this line
 module.exports = leapYears;

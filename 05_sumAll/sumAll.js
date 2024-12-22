4c6365b06ed22906991e75e7427d7c7ead211d02
xxh296 <140650758+xxh296@users.noreply.github.com>
2024-12-21 21:42:25 -0800

    Complete '05_sumAll' * Completed all 7 requirements (Jest tests) * Removed debug notes

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,28 +1,16 @@
 const sumAll = function(firstNumber, lastNumber) {
-    // formula : (n / 2) * (firstNumber + lastNumber)
-    // where n is the number of numbers in the range
-    // needs +1 correction, as most ranges don't start with 0
-    // TODO: a conditional to include 0
+    // formula : ((n / 2) + 1) * (firstNumber + lastNumber)
 
     if (firstNumber < 0 || lastNumber < 0) return "ERROR";
     if (!Number.isInteger(firstNumber+lastNumber)) return "ERROR"; 
-
     if (firstNumber > lastNumber) {
         const tempNumber = firstNumber;
         firstNumber = lastNumber;
         lastNumber = tempNumber;
     }
-    //debug
-    // console.log(lastNumber);
 
     return ((lastNumber - firstNumber + 1) / 2) * (firstNumber + lastNumber);
-
-
-
 };
-
-// const sum = sumAll(15, 10);
-// console.log(sum);
 
 // Do not edit below this line
 module.exports = sumAll;

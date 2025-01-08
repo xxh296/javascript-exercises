c1c5b95c05edfb8310eee1a1bfaebb5b31f1e237
xxh296 <140650758+xxh296@users.noreply.github.com>
2025-01-07 16:05:58 -0800

    Coomplete `08_calculator` * Added code for '`multiply', `'power`, and `factorial`.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,11 +1,9 @@
 const add = function(a, b) {
-  return a + b;
-	
+  return a + b;	
 };
 
 const subtract = function(a, b) {
-  return a - b;
-	
+  return a - b;	
 };
 
 const sum = function(...input) {
@@ -15,16 +13,29 @@ const sum = function(...input) {
     .reduce((total, item) => total + item, 0);	
 };
 
-const multiply = function() {
-
+const multiply = function(...input) {
+  return input.flat()
+    .map(item => Number(item))
+    .filter(item => !isNaN(item))
+    .reduce((total, item) => total * item, 1);	
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;	
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  a = +a;
+  if (a == 0) return 1;
+  if (a < 0 || typeof a !== "number" || !Number.isInteger(a)) 
+    return "ERROR: Provide a non-negative integer.";
+  
+  let result = 1;
+  while (a > 1) {
+    result *= a;
+    a--;     
+  }	
+  return result;
 };
 
 // Do not edit below this line

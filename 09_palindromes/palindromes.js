b1941ad1ab4a960a2157dce092ed2b9a4a4ea79e
xxh296 <140650758+xxh296@users.noreply.github.com>
2025-01-07 17:24:12 -0800

    Complete first 7 methods of `09_palindromes` * Pass for all cases, except for "unevenly spaced numbers".

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    input = input.toLowerCase().replace(/[^a-zA-Z]/g, "");
+    const reversedString = input.split("").reverse().join("");
+    return input === reversedString;
 };
+// console.log(palindromes("y2ah3ha2yZZZZ"));
 
 // Do not edit below this line
 module.exports = palindromes;

b165efdfa9187f8bcb0c4316cd1e02a3e5ed9e6a
xxh296 <140650758+xxh296@users.noreply.github.com>
2025-01-07 23:02:47 -0800

    Optimize `12_findTheOldest` * Refactored the the code for more clarity (changed a named function that was called only once).

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,21 +1,18 @@
 const findTheOldest = function(people) {
 
-    function insertYear(people) {
-        people.forEach(person => {
-            if (!person.yearOfDeath) {  // Check if yearOfDeath is missing (empty or undefined)
-                person.yearOfDeath = new Date().getFullYear();
-            }
-        });
-        return people; 
-    }
-
-    insertYear(people);    
+    people.forEach(person => {
+        if (!person.yearOfDeath) {  // Check if yearOfDeath is missing (empty or undefined)
+            person.yearOfDeath = new Date().getFullYear();
+        }
+    });
 
     people.sort((a, b) => 
         a.yearOfDeath - a.yearOfBirth >
         b.yearOfDeath - b.yearOfBirth ? -1 : 1);
-    return people[0];
+
+    return people[0]; 
 }
+
 
 // Do not edit below this line
 module.exports = findTheOldest;

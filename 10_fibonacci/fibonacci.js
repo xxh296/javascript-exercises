48138b9ab88c4598eef18f0141577f6f40d9fb47
xxh296 <140650758+xxh296@users.noreply.github.com>
2025-01-07 19:02:06 -0800

    Complete `10_fibonacci`

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,9 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    index = Number(index);
+    if (index < 0) return "OOPS";
+    if (index < 2) return index;
+    // let sequence = [];
+    return fibonacci(index - 1) + fibonacci(index - 2);
 };
 
 // Do not edit below this line

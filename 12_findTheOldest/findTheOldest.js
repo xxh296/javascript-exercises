85478d684ab9127650a9ca681f5c4cee09c4c1b4
xxh296 <140650758+xxh296@users.noreply.github.com>
2025-01-07 21:29:37 -0800

    Complete `12_findTheOldest` * Added the ability to calculate the age based on the current year, if a person's `yearOfDeath` is missing.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,9 +1,21 @@
 const findTheOldest = function(people) {
-    const sortedYearsLived = people.sort((a, b) => a.yearOfDeath - a.yearOfBirth
-        > b.yearOfDeath - b.yearOfBirth ? -1 : 1);
-    return people[0];
 
-};
+    function insertYear(people) {
+        people.forEach(person => {
+            if (!person.yearOfDeath) {  // Check if yearOfDeath is missing (empty or undefined)
+                person.yearOfDeath = new Date().getFullYear();
+            }
+        });
+        return people; 
+    }
+
+    insertYear(people);    
+
+    people.sort((a, b) => 
+        a.yearOfDeath - a.yearOfBirth >
+        b.yearOfDeath - b.yearOfBirth ? -1 : 1);
+    return people[0];
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
